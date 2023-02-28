5ae25d101be6169335ae67a3cf5e813b78bb0d28
Joonh <joon.im@student.csulb.edu>
2023-02-27 17:44:40 -0800

    commit1

--- app/src/main/java/com/example/mortgage_calculator/Mortgage.kt
+++ app/src/main/java/com/example/mortgage_calculator/Mortgage.kt
@@ -3,7 +3,6 @@ package com.example.mortgage_calculator
 import java.text.DecimalFormat
 
 class Mortgage {
-    val MONEY: DecimalFormat = DecimalFormat("$#,##0.00")
     val m: MainActivity = MainActivity()
 
     companion object {
@@ -75,35 +74,6 @@ class Mortgage {
     private var amount = 200000.0f
     private var years = 15
     private var rate = 0.035f
-
-    fun setAmount(newAmount: Float) {
-        if (newAmount >= 0) amount = newAmount
-    }
-
-    fun setYears(newYears: Int) {
-        if (newYears >= 0) years = newYears
-    }
-
-    fun setRate(newRate: Float) {
-        if (newRate >= 0) rate = newRate
-    }
-
-    fun getAmount(): Float {
-        return amount
-    }
-
-    fun getFormattedAmount(): String? {
-        return MONEY.format(amount)
-    }
-
-    fun getYears(): Int {
-        return years
-    }
-
-    fun getRate(): Float {
-        return rate
-    }
-
     fun monthlyPayment(): Float {
         val mRate = rate / 12 // monthly interest rate
         val temp = Math.pow(
@@ -113,24 +83,9 @@ class Mortgage {
         return amount * mRate / (1 - temp).toFloat()
     }
 
-    fun formattedMonthlyPayment(): String? {
-        if (amount == 0.0f)
-            return "$0.0"
-        else
-            return MONEY.format(monthlyPayment())
-    }
-
     fun totalPayment(): Float {
         return monthlyPayment() * years * 12
     }
-
-    fun formattedTotalPayment(): String? {
-        if (amount == 0.0f)
-            return "$0.0"
-        else
-            return MONEY.format(totalPayment())
-    }
-
 }
     private var amount = 200000.0f
     private var years =15
